a8ef325bede6aee4cfcc1d85ad6febeb8ffdb632
Burak Yigit Kaya <ben@byk.im>
2021-12-30 18:38:45 +0300

    Add link extraction for newsletters

--- functions/digest.ts
+++ functions/digest.ts
@@ -5,12 +5,76 @@ const makeHTML = (title: string, body: string) =>
   `<!DOCTYPE html>\n<html><head><meta charset="utf-8"/><meta name="viewport" content="width=device-width, initial-scale=1"/><title>${title}</title></head><body>${body}</body></html>`;
 const SUBJECT_CLEANER = /^((Re|Fwd):\s*)+/i;
 
-export const onRequest: PagesFunction<{
+type IPocketLetterEnv = {
   POCKET_CONSUMER_KEY: string;
   POCKET_TOKEN_KEY: string;
   SIGNING_SECRET: string;
   DATA: KVNamespace;
-}> = async ({request, env}) => {
+};
+
+class LinkFinder {
+  textMatcher =
+    /View (?:\w+ )*in (?:\w+ )*browser|(?:Read|View) (?:\w+ )*online|View (?:\w+ )*as (?:a )?web ?page/i;
+  href: string | null = null;
+  found = false;
+  textBuffer: string[] = [];
+
+  element(link: Element) {
+    if (this.found) {
+      return;
+    }
+
+    this.textBuffer = [];
+    this.href = link.getAttribute("href");
+  }
+
+  text(textChunk: Text) {
+    if (this.found || !this.href) {
+      return;
+    }
+
+    this.textBuffer.push(textChunk.text);
+
+    if (!textChunk.lastInTextNode) {
+      return;
+    }
+
+    const text = this.textBuffer.join("");
+    if (this.textMatcher.test(text)) {
+      this.found = true;
+    } else {
+      this.href = null;
+    }
+  }
+}
+
+async function extractDirectLink(html: string): Promise<string | null> {
+  const res = new Response(html);
+  const finder = new LinkFinder();
+
+  const sink = new HTMLRewriter().on('a[href^="http"]', finder).transform(res);
+  await sink.blob();
+
+  return finder.found ? (finder.href as string) : null;
+}
+
+async function storeLetter(
+  {env, request}: {env: IPocketLetterEnv; request: Request},
+  contents: string,
+): Promise<string> {
+  const fileName = `${await signature(contents, env.SIGNING_SECRET)}`;
+  await env.DATA.put(fileName, contents);
+  const url = new URL(request.url);
+  url.pathname = `/letter/${fileName}`;
+  url.search = "";
+
+  return url.toString();
+}
+
+export const onRequest: PagesFunction<IPocketLetterEnv> = async ({
+  request,
+  env,
+}) => {
   const data = await request.formData();
   const title = (data.get("subject") as string).replace(SUBJECT_CLEANER, "");
   const html = makeHTML(title, data.get("html") as string);
@@ -18,27 +82,20 @@ export const onRequest: PagesFunction<{
     "@",
     1,
   );
-  const fileName = `${await signature(html, env.SIGNING_SECRET)}.html`;
   const pocketToken = decrypt(fromName, env.POCKET_TOKEN_KEY);
 
-  // direct_link = BrowserLinkGetter()
-  // direct_link.feed(file_contents)
-  // if direct_link.found:
-  //     url = direct_link.link
-  //     print("Found direct link:", url)
-  // else:
-
-  await env.DATA.put(fileName, html);
-  const url = new URL(request.url);
-  url.pathname = `/letter/${fileName}`;
-  url.search = "";
+  const url =
+    (await extractDirectLink(html)) ||
+    (await storeLetter({env, request}, html));
+  console.log("Saving url:", url);
 
   const postData = {
-    url: url.toString(),
+    url,
     title,
     access_token: pocketToken,
     consumer_key: env.POCKET_CONSUMER_KEY,
   };
+
   const req = await fetch(POCKET_ADD_URL, {
     headers: {"Content-Type": "application/json"},
     method: "POST",
